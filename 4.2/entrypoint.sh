8fb0997f9db915bc16663dfdbaf6b6a68fa3650c
Gökhan Şengün <gokhansengun@gmail.com>
2019-12-06 08:58:29 +0300

    Fix springboot example 4.2

---
+++ 4.2/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Setting up stuff"
+echo "Setting up more stuff"
+echo "Setting up more and more stuff"
+echo "Ok, ready to run application"
+
+java -jar /app/spring-petclinic-2.2.0.BUILD-SNAPSHOT.jar
